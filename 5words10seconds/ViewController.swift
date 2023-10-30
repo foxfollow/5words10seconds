7a3f753acd2d4115a9c11f5361514365eba39a89
foxfollow <d3f0ld@gmail.com>
2023-10-30 14:04:29 +0200

    Initial Commit

---
+++ 5words10seconds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5words10seconds
+//
+//  Created by Heorhii Savoiskyi on 30.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
